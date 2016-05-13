202082d3b9daa6744bca2f7f3646fe3e92e826a2
Murali Manjunatha <Murali.Manjunatha@neudesic.com>
2016-05-13 20:33:55 +0530

    server js file updated

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-var express=require("express");
+var express = require("express");
 
-const app=express();
-var PORT=process.env.PORT || 3000;
+const app = express();
+var PORT = process.env.PORT || 3000;
 
-app.use(function(req,res,next)
-{
-  if(req.headers['x-forwarded-proto']==='http')
-  {
-    next();
-  }
-  else {
-    res.redirect("http://"+req.hostname +req.url);
-  }
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        res.redirect("http://" + req.hostname + req.url);
+    } else {
+        next();
+    }
 });
 
 app.use(express.static("public"));
 
-app.listen(PORT,function()
-{
-  console.log("server is up running on port "+ PORT);
+app.listen(PORT, function() {
+    console.log("server is up running on port " + PORT);
 })
