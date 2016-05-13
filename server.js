7cb4c5ece44b05e0153fa988594af06f568067e5
Murali Manjunatha <Murali.Manjunatha@neudesic.com>
2016-05-13 19:29:32 +0530

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express=require("express");
 
-var app=express();
+const app=express();
+var PORT=process.env.PORT ||3000;
+
+app.use(function(req,response,next)
+{
+  if(req.headers['x-forwarded-proto']==='http')
+  {
+    next();
+  }
+  else {
+    res.redirect("http://"+req.hostname +req.url);
+  }
+});
 
 app.use(express.static("public"));
 
-app.listen(3000,function()
+app.listen(PORT,function()
 {
-  console.log("server is up running on port 3000");
+  console.log("server is up running on port "+ PORT);
 })
