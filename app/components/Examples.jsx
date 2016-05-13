fb7d531e7e877173bd7562f7f896b822a5ba9ef3
Murali Manjunatha <Murali.Manjunatha@neudesic.com>
2016-05-13 17:46:59 +0530

    example page update

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,11 @@
 var React = require('react');
 var Examples = () => {
     return (
-        <h1>Examples component!</h1>
+      <div>
+          <h1>Examples component!</h1>
+          <p>This is exmaples page</p>
+      </div>
+
     )
 }
 
