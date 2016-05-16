bf605fc74194c8936f848cf0abc5adc26c8c0424
Murali Manjunatha <Murali.Manjunatha@neudesic.com>
2016-05-16 14:28:00 +0530

    fixed city temperature search issue

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require("react");
+var ReactDOM = require("react-dom");
+var ReactDOMServer = require("react-dom/server")
 
 var ErrorModal = React.createClass({
     propTypes: {
@@ -9,19 +11,24 @@ var ErrorModal = React.createClass({
         return {title: 'Error'}
     },
     componentDidMount: function() {
+      var {title, error} = this.props;
+      var htmlMarkup=(  <div className="reveal tiny text-center" id="error-modal" data-reveal="">
+            <h2>{title}</h2>
+            <p>{error}</p>
+            <p>
+                <button className="button hollow" data-close="">Okey</button>
+            </p>
+        </div>);
+        var $modal= $(ReactDOMServer.renderToString(htmlMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
         var modal = new Foundation.Reveal($('#error-modal'));
         modal.open();
     },
     render: function() {
-        var {title, error} = this.props;
 
         return (
-            <div className="reveal tiny text-center" id="error-modal" data-reveal="">
-                <h2>{title}</h2>
-                <p>{error}</p>
-                <p>
-                    <button className="button hollow" data-close="">Okey</button>
-                </p>
+            <div>
             </div>
         );
     }
