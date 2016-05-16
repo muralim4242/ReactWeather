74731c3a8c524f588ad324ed7b1322a5f77e7d56
Murali Manjunatha <Murali.Manjunatha@neudesic.com>
2016-05-16 18:40:18 +0530

    updated server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        res.redirect("http://" + req.hostname + req.url);
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
     } else {
         next();
     }
